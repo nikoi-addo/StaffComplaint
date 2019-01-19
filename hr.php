757a869b52ea5c9a9a39a71557cc10a61d6b9f5e
Rudy <rudyaddo@gmail.com>
2019-01-19 11:18:11 +0000

    Update hr.php

--- hr.php
+++ hr.php
@@ -184,7 +184,7 @@
                                      <div class="timeline-item-icon"><span class="fa fa-bullhorn"></span></div>
                                      <div class="timeline-item-content">
                                          <div class="timeline-heading">
-                                             <img src="assets/images/users/no-image.jpg"/> <b>Anonymus</b> <i>from</i> <u><?php echo $rows['c_division']; ?></u> made a complaint
+                                             <img src="assets/images/users/avatar.jpg"/> <b>Anonymus</b> <i>from</i> <u><?php echo $rows['c_division']; ?></u> made a complaint
                                          </div>
                                          <div class="timeline-body">
                                            <!-- Display Complaint from Database -->
@@ -229,7 +229,7 @@
                                           <!-- Comments -->
                                             <div class="timeline-body comments">
                                               <div class="comment-item">
-                                                  <img src="assets/images/users/no-image.jpg"/>
+                                                  <img src="assets/images/users/avatar.jpg"/>
                                                   <p class="comment-head">
                                                       <b>Human Resource Division</b>
                                                   </p>
