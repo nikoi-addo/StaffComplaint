6e12070b487bcb5c326e79e928b3e1c4347c4cbd
Rudy <rudyaddo@gmail.com>
2019-02-19 13:11:15 +0000

    admin

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@
 
 <head>
         <!-- META SECTION -->
-        <title>Internal Complaint - Admin</title>
+        <title>NCA Staff Ideas Portal - Admin</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
