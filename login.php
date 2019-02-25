138ba2acc2d906343d198ffba95b945699f34612
Mingleemma <mingleemma09@gmail.com>
2019-02-25 22:44:52 +0000

    Login

--- login.php
+++ login.php
@@ -1,30 +1,42 @@
 <!DOCTYPE html>
 <html lang="en" class="body-full-height">
-    
 
-<head>        
+
+<head>
         <!-- META SECTION -->
-        <title>NCA Staff Ideas Portal Login</title>            
+        <title>NCA Staff Ideas Portal Login</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
+
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
         <!-- END META SECTION -->
-        
-        <!-- CSS INCLUDE -->        
+
+        <!-- CSS INCLUDE -->
         <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
-        <!-- EOF CSS INCLUDE -->                                    
+        <!-- EOF CSS INCLUDE -->
     </head>
     <body>
-        
+
         <div class="login-container">
-        
+
             <div class="login-box animated fadeInDown">
                 <div class="login-logo"></div>
                 <div class="login-body">
+                  <?php if (isset($_POST['requestpassword'])){ ?>
+                    <form action="handlers/ops.php" method="post">
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <input type="e-mail" class="form-control" placeholder="Enter Staff Mail"/>
+                            <button class="btn btn-info btn-block" name="requestpassword">Request Passord</button>
+                        </div>
+                    </div>
+                    <form>
+                  <?php }
+                  else {
+                    ?>
                     <div class="login-title"><strong>Welcome</strong>, Please login</div>
-                    <form action="http://themifycloud.com/demos/templates/joli/index.html" class="form-horizontal" method="post">
+                    <form action="handlers/ops.php" class="form-horizontal" method="post">
                     <div class="form-group">
                         <div class="col-md-12">
                             <input type="e-mail" class="form-control" placeholder="E-mail"/>
@@ -36,12 +48,21 @@
                         </div>
                     </div>
                     <div class="form-group">
-                       
+
                         <div class="col-md-12">
                             <button class="btn btn-info btn-block">Log In</button>
+                            </form>
+                        <br>
+                        <form action="" method="post">
+                            <button class="btn btn-info btn-block" name="requestpassword">First Login? Request Password</button>
+                          </form>
                         </div>
                     </div>
-                    </form>
+                <?php
+                  }
+                ?>
+
+
                 </div>
                 <div class="login-footer">
                     <div class="pull-left">
@@ -54,16 +75,10 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
-        
+
     </body>
 
 
 </html>
-
-
-
-
-
-
