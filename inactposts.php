1f6dacae81751624ea21923d817cd34475c7a7e0
Rudy <rudyaddo@gmail.com>
2019-06-29 01:29:41 +0100

    removed IP tables

--- inactposts.php
+++ inactposts.php
@@ -143,7 +143,7 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['adminpanel'])) {
                                                 <th width="300">Post</th>
                                                 <th width="100">Reply from HR</th>
                                                 <th width="100">Status</th>
-                                                <th width="100">IP Address</th>
+                                               
                                                 <th width="100">Date</th>
                                                 <th width="100">Actions</th>
                                             </tr>
@@ -207,7 +207,7 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['adminpanel'])) {
                                                       <?php } ?>
                                                     </td>
                                                   <td><?php echo "<span class='label label-danger'>Inactive</span>";?></td>
-                                                  <td><?php echo $rows['c_ip_address']; ?></td>
+                                                  
                                                   <td><?php echo date("M d, Y @ h:i a", $rows['date_created']); ?></td>
                                                   <td>Yet to decide</td>
                                                 </tr>
