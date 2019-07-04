3d4396343970828849c45647a90869ac0f35733d
Rudy <rudyaddo@gmail.com>
2019-07-05 00:51:04 +0100

    front end changes

--- message.php
+++ message.php
@@ -41,20 +41,81 @@
             <!-- PAGE CONTENT -->
             <div class="page-content">
 
-                <!-- START X-NAVIGATION VERTICAL -->
+               <!-- START X-NAVIGATION VERTICAL -->
                 <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
 
-                    <li href="index.php" class="xn-icon-button pull-left">
+                  <li class="xn-icon-button pull-left">
                        <img src="img/logo.png"/>
 
                     </li>
 
+                    <?php
+                    //Query to extract array of unreadmessages
+                    $sql_unreadmessages = "SELECT * FROM login_info WHERE no=$user_id";
+                    $success_unreadmessages = mysqli_query($link, $sql_unreadmessages);
+                    if ($success_unreadmessages) {
+                      $unreadmessages = $success_unreadmessages->fetch_assoc();
+                      $listunreadmessages = $unreadmessages['u_unreadmessage'];
+                      $listunreadmessages = explode("|", $listunreadmessages);
+                      if ($listunreadmessages !== "") {
+                        $countunreadmessages = 0;
+                      }
+                      else {
+                        $countunreadmessages = count($listunreadmessages);
+                      }
+                    }
+
+                    ?>
+
+                    <!-- MESSAGES -->
+                    <li class="xn-icon-button pull-right">
+                        <a href="#mb-signout"><span class="fa fa-inbox"></span></a>
+                        <div class="informer informer-danger"><?php echo $countunreadmessages; ?></div>
+                        <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><span class="fa fa-inbox"></span> Messages From HR</h3>
+                                <div class="pull-right">
+                                    <span class="label label-danger"></span>
+                                </div>
+                            </div>
+                            <div class="panel-body list-group list-group-contacts scroll" style="height: 200px;">
+
+                             <?php
+                                //SQL Query to show messages
+                                $sql_showhrmessages = "SELECT * FROM messagehr ORDER BY m_date_created DESC";
+                                //Execture show HR messages query
+                                $success_showhrmessages = mysqli_query($link, $sql_showhrmessages);
+                                // Get users unreadmessages and put in an array
+
+                                //Check if execution returned true
+                                if ($success_showhrmessages->num_rows > 0) {
+                                    while($mrows = $success_showhrmessages->fetch_assoc()){
+                                      $msgid = $mrows['m_id'];?>
+
+                                      <a href="message.php?msgid=<?php echo $mrows['m_id']; ?>" class="list-group-item">
+                                          <div class="list-group-status <?php if (in_array($msgid, $listunreadmessages)) {echo "status-online";} else{echo "status-offline";}?>"></div>
+                                          <span class="contacts-title"><?php echo $mrows['m_subject']; ?></span>
+                                          <!-- Use the substr() function to get a specific length of the message to show -->
+                                          <p><?php echo substr($mrows['m_message'], 0, 110) . " ...";?></p>
+                                      </a>
+
+                                <?php }
+                                }
+                                else {
+                                  echo "Unable to Show Messages";
+                                }
 
 
+                              ?>
+
+                        </div>
+                    </li>
+                    <!-- END MESSAGES -->
+
+                  
 
                 </ul>
                 <!-- END X-NAVIGATION VERTICAL -->
-
                 <!-- START BREADCRUMB -->
                 <ul class="breadcrumb push-down-0">
                     <li><a href="index.php">Home</a></li>
@@ -64,7 +125,7 @@
                 <!-- END BREADCRUMB -->
 
                 <!-- START CONTENT FRAME -->
-                <div class="content-frame">
+                <div class="content-frame col-md-12">
                     <!-- START CONTENT FRAME TOP -->
                     <div class="content-frame-top">
                         <div class="page-title">
@@ -92,11 +153,11 @@
                         //Query to update unread messages
                         $sql_updtunreadmessages = "UPDATE login_info SET u_unreadmessage = '$finalunreadmessages' WHERE no = $user_id";
                         $success_updtunreadmessages = mysqli_query($link, $sql_updtunreadmessages);
-<<<<<<< HEAD
 
-=======
+
+
                         echo mysqli_query($link , $success_updtunreadmessages);
->>>>>>> ea04e11a1a118713fa332a5a4fa0d353a0e2a701
+
                       }
                     }
 
@@ -109,9 +170,9 @@
                       while($rows = $success_displaymessage->fetch_assoc()){ ?>
 
                     <!-- START CONTENT FRAME BODY -->
-                    <div class="content-frame-body">
+                    <div class="page-content-wrap">
                           <div class="panel panel-default">
-                              <div class="panel-heading">
+                              <div class="panel-heading ">
                                   <!--<div class="pull-left">
                                       <img src="assets/images/users/avatar.jpg" class="panel-title-image" alt="John Doe"/>
                                       <h3 class="panel-title"><?php echo $rows['m_division']; ?></h3>
